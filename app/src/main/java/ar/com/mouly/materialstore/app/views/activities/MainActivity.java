0ff0298439bc68c4f35c0b61fef35b7e20aa27e7
Adrián Mouly <adrian@mouly.com.ar>
2015-03-29 23:01:41 -0300

    Adding Navigation Drawer.

--- app/src/main/java/ar/com/mouly/materialstore/app/views/activities/MainActivity.java
+++ app/src/main/java/ar/com/mouly/materialstore/app/views/activities/MainActivity.java
@@ -2,15 +2,50 @@ package ar.com.mouly.materialstore.app.views.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import ar.com.mouly.materialstore.app.R;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * @author amouly on 3/29/15.
  */
 public class MainActivity extends ActionBarActivity {
 
+    @InjectView(R.id.toolbar_main) Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(toolbar);
+
+        Drawer.Result result = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_home),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+
+                    }
+                })
+                .build();
     }
 }
