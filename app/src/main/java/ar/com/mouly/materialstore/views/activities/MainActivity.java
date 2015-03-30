e607cfbce7a8b18450ebe41f683607bc27334ad8
Adrián Mouly <adrian@mouly.com.ar>
2015-03-29 21:11:04 -0300

    Create Main Activity foundation.

---
+++ app/src/main/java/ar/com/mouly/materialstore/views/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package ar.com.mouly.materialstore.views.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * @author amouly on 3/29/15.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
