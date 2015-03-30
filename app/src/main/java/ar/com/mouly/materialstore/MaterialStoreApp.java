3d1847f3e32d154e317220e4217a1b20ac735e88
Adrián Mouly <adrian@mouly.com.ar>
2015-03-29 21:19:12 -0300

    Create Application structure.

---
+++ app/src/main/java/ar/com/mouly/materialstore/MaterialStoreApp.java
@@ -0,0 +1,14 @@
+package ar.com.mouly.materialstore;
+
+import android.app.Application;
+
+/**
+ * @author amouly on 3/29/15.
+ */
+public class MaterialStoreApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
