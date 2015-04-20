db9707cbc7f156fbeb95f42f9db32281d8227d6c
Adrián Mouly <adrian@mouly.com.ar>
2015-04-19 21:51:34 -0300

    Structure for Data entity.

--- model/src/main/java/ar/com/mouly/materialstore/model/entities/Data.java
+++ model/src/main/java/ar/com/mouly/materialstore/model/entities/Data.java
@@ -1,7 +1,21 @@
 package ar.com.mouly.materialstore.model.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * @author amouly on 4/19/15.
  */
 public class Data {
+    @SerializedName("id")
+    private int id;
+    @SerializedName("code")
+    private String code;
+    @SerializedName("name")
+    private String name;
+    @SerializedName("url")
+    private String url;
+    @SerializedName("slug")
+    private String slug;
+
+
 }
